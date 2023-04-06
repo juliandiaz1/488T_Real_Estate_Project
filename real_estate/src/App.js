4ad35f2b65deeb108e11993741df0764a26ea8d1
Brandon Soncarty <bsoncarty@ewu.edu>
2023-04-06 11:13:50 -0700

    Added simple navigation links to App

--- real_estate/src/App.js
+++ real_estate/src/App.js
@@ -1,9 +1,32 @@
 import Navigation from "./components/Navigation";
+import {Link, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Account from "./pages/Account";
+import Houses from "./pages/Houses";
 function App() {
 
-    return<div>
-        <Navigation />
-        </div>
+    return (
+        <>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/account">Account</Link>
+                </li>
+                <li>
+                    <Link to="/houses">Houses</Link>
+                </li>
+            </ul>
+        </nav>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/account" element={<Account />}/>
+            <Route path="/houses" element={<Houses />}/>
+        </Routes>
+        </>
+    )
 }
 
 export default App;
