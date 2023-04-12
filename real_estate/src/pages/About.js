a17c602f7969cde24cde3036eb6f3bc91a2212ee
Brandon Soncarty <bsoncarty@ewu.edu>
2023-04-12 15:38:35 -0700

    spaced out pics into columns and added about text

--- real_estate/src/pages/About.js
+++ real_estate/src/pages/About.js
@@ -6,12 +6,40 @@ function About() {
     return (
         <div>
             <h1>About Page</h1>
-            <div>
-                <figure className="image is-128x128">
-                    <img className="is-rounded" src={Brandon} alt="a picture of Brandon" />
-                    <img className="is-rounded" src={Cason} alt="a picture of Cason" />
-                    <img className="is-rounded" src={Hannah} alt="a picture of Hannah" />
-                </figure>
+            <div className="container is-fluid">
+                <div className="notification">
+                    The purpose of this project is to create a user-friendly website that customers can use to filter properties from the
+                    MLS and Property Owners to determine what ones will provide the best ROI.
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column is-3 ml-6">
+                    <figure className="image is-128x128">
+                        <img className="is-rounded" src={Brandon} alt="a picture of Brandon" />
+                        <div className="subtitle has-text-centered">Brandon Soncarty</div>
+                    </figure>
+                </div>
+
+                <div className="column is-3">
+                    <figure className="image is-128x128">
+                        <img className="is-rounded" src={Cason} alt="a picture of Cason" />
+                        <div className="subtitle has-text-centered">Cason Nichols</div>
+                    </figure>
+                </div>
+
+                <div className="column is-3">
+                    <figure className="image is-128x128">
+                        <img className="is-rounded" src={Hannah} alt="a picture of Hannah" />
+                        <div className="subtitle has-text-centered">Hannah Kosmicki</div>
+                    </figure>
+                </div>
+
+                <div className="column is-3">
+                    <figure className="image is-128x128">
+                        <img className="is-rounded" src={Hannah} alt="a picture of Hannah" />
+                        <div className="subtitle has-text-centered">Hannah Kosmicki</div>
+                    </figure>
+                </div>
             </div>
         </div>
 )
