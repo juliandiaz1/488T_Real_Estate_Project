ffee60fbef4280fc5540e2791af31a77bb2ca8b5
Cason Nichols <cnichols8@ewu.edu>
2023-04-18 18:28:27 -0700

    Worked on account page.

---
+++ real_estate/src/components/User.js
@@ -0,0 +1,61 @@
+import React from "react";
+import '../styles/User.css';
+
+
+function User(props){
+
+    const display_user = (props) => {
+        let users = props.info;
+        
+        console.log(Object.keys(users));
+        if(Object.keys(users).length > 0){
+            return(
+                Object.entries(users.data).map(([key, user]) => {
+                    
+                    return (
+                        <form key={key} className="box">
+                            <div className="field">
+                            <label className="label">First Name</label>
+                            <div class="control">
+                                <input class="input" type="name" placeholder={user.fname}></input>
+                            </div>
+                            </div>
+                            <div className="field">
+                            <label className="label">Last Name</label>
+                            <div class="control">
+                                <input class="input" type="name" placeholder={user.lname}></input>
+                            </div>
+                            </div>
+                            <div className="field">
+                            <label className="label">Email</label>
+                            <div class="control">
+                                <input class="input" type="email" placeholder={user.email}></input>
+                            </div>
+                            </div>
+                            <div className="field">
+                            <label className="label">Phone Number</label>
+                            <div class="control">
+                                <input class="input" type="name" placeholder={user.phone_number}></input>
+                            </div>
+                            </div>
+                        </form>
+                    )
+                })
+            )
+        }else{
+            return(
+                <h1>Not logged in.</h1>
+            )
+        }
+        
+        
+    }
+
+    return(
+        <>
+            {display_user(props)};
+        </>
+    )
+}
+
+export default User;
--- real_estate/src/pages/Account.js
+++ real_estate/src/pages/Account.js
@@ -1,5 +1,44 @@
+<<<<<<< Updated upstream
 function Account(){
     return <div>Account Page</div>
+=======
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+import User from '../components/User';
+import '../styles/Account.css';
+
+function Account(){
+
+    const [userinfo, setUserInfo] = useState('');
+    
+    const display_info = async () => {
+        await axios({
+            method: "get",
+            url: "http://localhost:3001/get_account",
+            withCredentials: true,
+        }).then(res => {
+            
+            setUserInfo(res)});
+    }
+
+
+    useEffect(() => {
+        display_info();
+    }, []);
+
+    return (
+        <div>
+             <div className="account-cntr">
+                <User info={userinfo} />
+            </div>
+            <div>
+        <form className="box" id="User-box">
+            <p>Random text</p>
+        </form>
+        </div>
+        </div>
+    )
+>>>>>>> Stashed changes
 }
 
 export default Account;
